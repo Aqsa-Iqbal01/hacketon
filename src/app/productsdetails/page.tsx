7e595cab694bc0a746e18f370a85533d3bc46025
Aqsa Iqbal <164233800+Aqsa-Iqbal01@users.noreply.github.com>
2024-12-09 01:33:33 +0500

    updated index.html

--- src/app/productsdetails/page.tsx
+++ src/app/productsdetails/page.tsx
@@ -2,7 +2,7 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from "next/link";
-import { Card,CardContent,CardFooterg,} from "@/components/ui/card";
+import { Card,CardContent,CardFooter,} from "@/components/ui/card";
 
  
 export default function Page() {
